1d5e16843a1909629f4c0aa7f2402b27e804a814
Shreyas Atre <61797109+SAtacker@users.noreply.github.com>
2020-04-02 21:14:04 +0530

    Updated code with commands

--- arduino_esp_sketch/arduino_esp_sketch.ino
+++ arduino_esp_sketch/arduino_esp_sketch.ino
@@ -1,4 +1,10 @@
-
+/////Note this is a test code
+/////The only purpose is to achieve a communication between arduino and and the given app
+/////the following error occurs
+////rst:0xc (SW_CPU_RESET),boot:0x13 (SPI_FAST_FLASH_BOOT)
+////guru meditation error: core 1 panic'ed (loadprohibited). exception was unhandled.
+//The above error keeps on occuring
+//However if line 51(Code to print command) is removed then it vanishes
 #include <WiFi.h>
 
 #include <WiFiClient.h>
@@ -9,49 +15,44 @@
 
 
 
-const char* ssid = "ESP";
-const char* password = "987654321";
+const char* ssid = "ESP";               //SSID 
+const char* password = "987654321";     //Password
 
 
-WebServer server(80);
+WebServer server(80);                   //Server at port 80
 void poweron(){
   Serial.println("Device power on");
 }
 
 
 void setup(void) {
-  Serial.begin(115200);
-  WiFi.mode(WIFI_STA);
-  WiFi.softAP(ssid, password);
+  Serial.begin(115200);                            
+  WiFi.softAP(ssid, password);      //Start AP
   Serial.println("");
 
 
-  server.on("/power/on",poweron);
+  server.on("/power/on",poweron);   //Handle server request
 
 
 
-  server.begin();
+  server.begin();                   //Start Server
   Serial.println("HTTP server started");
   IPAddress IP = WiFi.softAPIP();
-  Serial.print("AP IP address: ");
+  Serial.print("AP IP address: ");       
+  //Displays ipaddress
   Serial.println(IP);
   
 }
 
 void loop(void) {
-  server.handleClient();
-  WiFiClient client;
-   char command = client.read(); 
-        
-        
-        if (command == 'H') {
-          
-          
-          Serial.println("Led is now on.");
-        }
-        else if (command == 'L') {
-          
-          
+  server.handleClient();                      //handle server requests
+  WiFiClient client;                          
+   char command = client.read();              //Read incoming messages
+   Serial.println(command);                   //It prints the incoming char
+   if (command == 'H') {                       //If incoming char has ASCII value of H
+          Serial.println("Led is now on.");    //Print LED is on on serial monitor
+   }
+   else if (command == 'L') {
           Serial.println("Led is now off.");
-        }        
+   }        
 }
